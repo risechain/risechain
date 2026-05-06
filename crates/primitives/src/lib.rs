798aade43d73ef435bbfd6076112134bb4c5ba37
phuocthanhdo <phuocthanhdo@yahoo.com>
2026-05-06 13:51:15 +0700

    feat: impl core IPC primitives (#2)

--- crates/primitives/src/lib.rs
+++ crates/primitives/src/lib.rs
@@ -1,2 +1,8 @@
 mod rpc;
 pub use rpc::{RiseRpcLog, RiseRpcTransactionReceipt};
+
+mod ipc;
+pub use ipc::{
+    RiseIpcClient, RiseIpcClientError, RiseIpcConnection, RiseIpcReceipt, RiseIpcReceiptLog,
+    RiseIpcRequest, RiseIpcResponse, RiseIpcTransportError,
+};
